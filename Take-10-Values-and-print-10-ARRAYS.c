e16dc16c0b803ab39194ca1a5d0f31502fe15468
Darsh Gupta <darshgupta548@gmail.com>
2025-09-09 09:59:52 +0530

    Create Take-10-Values-and-print-10-ARRAYS.c

---
+++ Take-10-Values-and-print-10-ARRAYS.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int a[10],i;
+  printf("Enter 10 Numbers\n");
+  
+  for(i=0; i<=9; i++){
+      scanf("%d",&a[i]);
+  }
+      printf("\n");
+      printf("%d \n",a[3]);
+      printf("%d \n",a[6]);
+      printf("%d \n",a[8]);
+    
+ 
+    return 0;
+}
