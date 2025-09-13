4a69a81f80ed5c72b1dc7093eec47e51b2edeb8a
Darsh Gupta <darshgupta548@gmail.com>
2025-09-13 23:27:35 +0530

    Update Sum_OF_ODD_Nos.c

--- Sum_OF_ODD_Nos.c
+++ Sum_OF_ODD_Nos.c
@@ -5,10 +5,11 @@ int n, sum = 0;
 printf("Enter a number \n");
 scanf("%d", &n);
 for(int i=1 ; i<=n; i++) { 
-   sum = sum + (2*i)+1;
+   sum = sum + (2*i)-1;
 }
 
 printf("The Sum of First %d Odd Numbers is %d", n, sum);
 
 return 0;
+
 }
