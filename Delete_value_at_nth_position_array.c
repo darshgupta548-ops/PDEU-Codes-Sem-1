d553f578850334e3a2b92a3865b1898c8285b9b1
Darsh Gupta <darshgupta548@gmail.com>
2025-10-02 22:36:19 +0530

    Update Delete_value_at_nth_position_array.c

--- Delete_value_at_nth_position_array.c
+++ Delete_value_at_nth_position_array.c
@@ -3,19 +3,15 @@
 int main() {
     int a[5], i, index;
 
-    printf("Enter 5 numbers (use 0 as empty slots):\n");
+    printf("Enter 5 numbers\n");
     for(i = 0; i < 5; i++) {
         scanf("%d", &a[i]);
     }
 
-    printf("Enter the index of the number to delete (0-4):\n");
+    printf("Enter the index of the number to delete\n");
     scanf("%d", &index);
 
-    for(i = index; i < 4; i++) {
-        a[i] = a[i + 1];
-    }
-
-    a[4] = 0;
+    a[index] = 0;
 
     printf("\n");
 
